bfad084e80535670c5db4e0397fb911f9587bbbd
Pauolo <pauolosavery1@gmail.com>
2025-09-30 20:40:34 +0300

    [FE] - добавил папку frontend

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Reports } from "./Reports/index";
-export { default as History } from "./History/index";
-export { default as Main } from "./Main/index";
-export { default as Account } from "./Account/index";
-export { default as AuthPage } from "./AuthPage/index";
