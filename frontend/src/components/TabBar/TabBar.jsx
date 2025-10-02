a0df8e8e8d142d06678f495b4dffeca9108827cf
Karis Dev <karjaakirill@gmail.com>
2025-10-02 22:18:22 +0300

    map

---
+++ frontend/src/components/TabBar/TabBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './TabBar.css';
+import houseIcon from '../../images/maps-markers/house_icon.svg';
+import itpIcon from '../../images/maps-markers/itp_icon.svg';
+
+const TabBar = ({ activeLayers, onLayerToggle }) => {
+  return (
+    <div className="tabbar-container">
+      <div className="tabbar">
+        <button
+          className={`tab-button ${activeLayers.itp ? 'active' : ''}`}
+          onClick={() => onLayerToggle('itp')}
+        >
+          <div className="tab-icon itp-icon">
+            <img src={itpIcon} alt="ITP" />
+          </div>
+          <span className="tab-label">ИТП</span>
+        </button>
+        
+        <button
+          className={`tab-button ${activeLayers.mkd ? 'active' : ''}`}
+          onClick={() => onLayerToggle('mkd')}
+        >
+          <div className="tab-icon mkd-icon">
+            <img src={houseIcon} alt="MKD" />
+          </div>
+          <span className="tab-label">МКД</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TabBar;
